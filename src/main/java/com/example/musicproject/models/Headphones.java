c90cdd393eb35fbd3d2573fb6f8fa7e242f1bbe0
Daniel Svirida <danielsvirida@gmail.com>
2023-02-03 13:01:16 -0800

    Model Files

---
+++ src/main/java/com/example/musicproject/models/Headphones.java
@@ -0,0 +1,17 @@
+package com.example.musicproject.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Headphones {
+    private String name;
+    private double price;
+    private double overallRating;
+    private double bassRating;
+}
