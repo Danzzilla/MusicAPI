94b13e81c8ce40211ffc42aa6a1dffad7efcd2f0
Daniel Svirida <danielsvirida@gmail.com>
2023-02-03 13:36:41 -0800

    Service Files

---
+++ src/main/java/edu/greenriver/sdev/musicproject/services/HeadphonesService.java
@@ -0,0 +1,73 @@
+package edu.greenriver.sdev.musicproject.services;
+
+import edu.greenriver.sdev.musicproject.models.Headphones;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class HeadphonesService {
+    private List<Headphones> headphones = new ArrayList<>(List.of(
+            Headphones.builder()
+                    .brand("Bose")
+                    .model("Quietcomfort 35")
+                    .price(235)
+                    .rating(4.8)
+                    .build(),
+            Headphones.builder()
+                    .brand("Skullcandy")
+                    .model("Crusher")
+                    .price(200)
+                    .rating(4.2)
+                    .build()
+    ));
+
+    //GET requests (read)
+
+    public List<Headphones> allHeadphones(){
+        return headphones;
+    }
+
+    public Headphones findHeadphonesByModel(String modelName){
+        return headphones.stream()
+                .filter(rec -> rec.getModel().equalsIgnoreCase(modelName))
+                .findFirst()
+                .orElse(null);
+    }
+
+    //POST requests (create)
+
+    public Headphones addHeadphones(Headphones newHeadphones){
+        headphones.add(newHeadphones);
+        return newHeadphones;
+    }
+
+    //PUT requests (update)
+
+    public Headphones updateHeadphones(Headphones updatedHeadphones){
+        Headphones found = findHeadphonesByModel(updatedHeadphones.getModel());
+
+        if(found != null){
+            found.setBrand(updatedHeadphones.getBrand());
+            found.setModel(updatedHeadphones.getModel());
+            found.setPrice(updatedHeadphones.getPrice());
+            found.setRating(updatedHeadphones.getRating());
+        }
+
+        return found;
+    }
+
+    //DELETE requests (delete)
+
+    public void deleteHeadphones(String modelName){
+        //filter out just the matching model name
+        headphones = headphones.stream()
+                .filter(rec -> !rec.getModel().equalsIgnoreCase(modelName))
+                .toList();
+    }
+
+    public boolean areValidHeadphones(Headphones headphones) {
+        return headphones.getModel() != null && !headphones.getModel().isEmpty();
+    }
+}
---
+++ src/main/java/edu/greenriver/sdev/musicproject/services/MusicService.java
@@ -0,0 +1,67 @@
+package edu.greenriver.sdev.musicproject.services;
+
+import edu.greenriver.sdev.musicproject.models.Music;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class MusicService {
+    private List<Music> music = new ArrayList<>(List.of(
+            Music.builder()
+                    .name("Riptide")
+                    .songWriter("Vance Joy")
+                    .length(3.24)
+                    .year(2013)
+                    .build()
+    ));
+
+    //GET requests (read)
+
+    public List<Music> allMusic(){
+        return music;
+    }
+
+    public Music findMusicByName(String songName){
+        return music.stream()
+                .filter(rec -> rec.getName().equalsIgnoreCase(songName))
+                .findFirst()
+                .orElse(null);
+    }
+
+    //POST requests (create)
+
+    public Music addMusic(Music newMusic){
+        music.add(newMusic);
+        return newMusic;
+    }
+
+    //PUT requests (update)
+
+    public Music updateMusic(Music updatedMusic){
+        Music found = findMusicByName(updatedMusic.getName());
+
+        if(found != null){
+            found.setName(updatedMusic.getName());
+            found.setSongWriter(updatedMusic.getSongWriter());
+            found.setLength(updatedMusic.getLength());
+            found.setYear(updatedMusic.getYear());
+        }
+
+        return found;
+    }
+
+    //DELETE requests (delete)
+
+    public void deleteMusic(String songName){
+        //filter out just the matching song name
+        music = music.stream()
+                .filter(rec -> !rec.getName().equalsIgnoreCase(songName))
+                .toList();
+    }
+
+    public boolean isValidMusic(Music music) {
+        return music.getName() != null && !music.getName().isEmpty();
+    }
+}
