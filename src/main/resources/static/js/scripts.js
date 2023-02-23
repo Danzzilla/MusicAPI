94d5a2389e8c9ff8d8cc49ba3c5e1ce48f319ef3
Daniel Svirida <danielsvirida@gmail.com>
2023-02-23 05:45:18 -0800

    Split into multiple files for cleaner code

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -12,6 +12,55 @@
 //     $('.dataTables_length').addClass('bs-select');
 // });
 
+//Initiators
+
+load("music");
+load("headphones");
+
+document.querySelector("#submit-music").addEventListener("click", event => {
+    let jsobj = {};
+
+    let name = document.querySelector("#name");
+    let writer = document.querySelector("#writer");
+    let length = document.querySelector("#length");
+    let year = document.querySelector("#year");
+
+    jsobj.name = name.value;
+    jsobj.songWriter = writer.value;
+    jsobj.length = length.value;
+    jsobj.year = year.value;
+    jsobj.id = Math.floor(Math.random() * Date.now());
+
+    add("music", jsobj);
+
+    document.querySelector("#name").value = "";
+    document.querySelector("#writer").value = "";
+    document.querySelector("#length").value = "";
+    document.querySelector("#year").value = "";
+})
+
+document.querySelector("#submit-headphones").addEventListener("click", event => {
+    let jsobj = {};
+
+    let brand = document.querySelector("#brand");
+    let model = document.querySelector("#model");
+    let price = document.querySelector("#price");
+    let rating = document.querySelector("#rating");
+
+    jsobj.brand = brand.value;
+    jsobj.model = model.value;
+    jsobj.price = price.value;
+    jsobj.rating = rating.value;
+    jsobj.id = Math.floor(Math.random() * Date.now());
+
+    add("headphones", jsobj);
+
+    document.querySelector("#brand").value = "";
+    document.querySelector("#model").value = "";
+    document.querySelector("#price").value = "";
+    document.querySelector("#rating").value = "";
+})
+
 document.querySelector("#add-music").addEventListener("click", event => {
     let card = document.getElementById("music-card");
 
